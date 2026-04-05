ca55aa7db5ae712c44a85e2d1ed66f8521cccc6d
Deepanshu <deepanshugoyal1264@gmail.com>
2026-04-05 13:08:06 +0530

    Time: 5 ms (68.18%), Space: 46.3 MB (15.03%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0,y=0;
+        for(char c:moves.toCharArray()){
+            if(c=='R')x++;
+            if(c=='L')x--;
+            if(c=='U')y++;
+            if(c=='D')y--;
+        }
+        return (x==0 &&y==0);
+    }
+}
