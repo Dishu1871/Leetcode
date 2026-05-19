aa4ffeb571a571df57176cc0590f4f5bb04180cd
Deepanshu <deepanshugoyal1264@gmail.com>
2026-05-19 23:29:56 +0530

    Time: 1 ms (98.36%), Space: 80.3 MB (63.48%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int n1=nums1.length, n2=nums2.length;
+        int p1=0, p2=0;
+        for( ; p1<n1 && p2<n2; ){
+            int x=nums1[p1], y=nums2[p2];
+            if (x==y) return x;
+            else if (x>y) p2++;
+            else p1++;
+        }
+        return -1;
+    }
+}
